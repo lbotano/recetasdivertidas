e92ffaab2f24c73236618b1933a7e4fca0749c77
lbotano <otanolautarob@gmail.com>
2020-09-19 22:02:00 -0300

    Pasados los stored procedures a MySQL

--- db/01 Tablas.sql
+++ db/01 Tablas.sql
@@ -1,38 +1,46 @@
---CREATE DATABASE RecetasDivertidasDB;
+CREATE DATABASE IF NOT EXISTS RecetasDivertidasDB;
 USE RecetasDivertidasDB;
 
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS Usuario;
 CREATE TABLE Usuario(
 	uNickname varchar(32) NOT NULL,
 	uPreguntaSeguridad varchar(64) NOT NULL,
 	uRespuestaSeguridad varchar(64) NOT NULL,
-	uNombre varchar NOT NULL,
-	uApellido varchar NOT NULL,
+	uNombre varchar(50) NOT NULL,
+	uApellido varchar(50) NOT NULL,
 	uContrasenia varchar(50) NOT NULL,
-	uGenero bit,
+	uGenero boolean,
 	uMail varchar(64) NOT NULL,
+    uHabilitado boolean DEFAULT true, 
 	CONSTRAINT pk_u PRIMARY KEY (uNickname),
 	CONSTRAINT uk_um UNIQUE (uMail)
 );
 
+DROP TABLE IF EXISTS Ingrediente;
 CREATE TABLE Ingrediente(
 	iID int,
-	iNombre varchar UNIQUE,
+	iNombre varchar(64) UNIQUE,
 	CONSTRAINT pk_i PRIMARY KEY (iID)
 );
 
+DROP TABLE IF EXISTS CategoriaDeIngrediente;
 CREATE TABLE CategoriaDeIngrediente (
 	cID int,
-	cNombre varchar,
+	cNombre varchar(50),
 	CONSTRAINT pk_ci PRIMARY KEY (cID),
 	CONSTRAINT uk_ci UNIQUE (cNombre)
 );
 
+DROP TABLE IF EXISTS CategoriaDeReceta;
 CREATE TABLE CategoriaDeReceta(
 	cID int,
 	cNombre varchar(64),
 	CONSTRAINT pk_cr PRIMARY KEY (cID)
 );
 
+DROP TABLE IF EXISTS RelCatIngred;
 CREATE TABLE RelCatIngred(
 	iID int,
 	cID int,
@@ -41,7 +49,7 @@ CREATE TABLE RelCatIngred(
 	CONSTRAINT fk_cid FOREIGN KEY (iID) REFERENCES CategoriaDeIngrediente(cID) 
 );
 
-
+DROP TABLE IF EXISTS Receta;
 CREATE TABLE Receta (
 	rID int,
 	rNombre varchar (128),
@@ -49,6 +57,7 @@ CREATE TABLE Receta (
 	CONSTRAINT pk_r PRIMARY KEY (rID)
 );
 
+DROP TABLE IF EXISTS RelCatReceta;
 CREATE TABLE RelCatReceta (
 	rID int,
 	cID int,
@@ -57,6 +66,7 @@ CREATE TABLE RelCatReceta (
 	CONSTRAINT fk_rcrcID FOREIGN KEY (cID) REFERENCES CategoriaDeReceta(cID)
 );
 
+DROP TABLE IF EXISTS IngredienteReceta;
 CREATE TABLE IngredienteReceta (
 	rID int,
 	iID int,
@@ -67,21 +77,24 @@ CREATE TABLE IngredienteReceta (
 	CONSTRAINT fk_iriID FOREIGN KEY (iID) REFERENCES Ingrediente(iID)
 );
 
+DROP TABLE IF EXISTS Multimedia;
 CREATE TABLE Multimedia (
-	mID int,
-	link text,
-	rID int,
+	mID int NOT NULL AUTO_INCREMENT,
+	link text NOT NULL,
+	rID int NOT NULL,
 	CONSTRAINT pk_mID PRIMARY KEY (mID),
 	CONSTRAINT fk_mrID FOREIGN KEY (rID) REFERENCES Receta(rID)
 );
 
-
+DROP TABLE IF EXISTS Calificacion;
 CREATE TABLE Calificacion (
-	uNickname varchar(32),
-	rID int,
-	calificac�on tinyint,
+	uNickname varchar(32) NOT NULL,
+	rID int NOT NULL,
+	calificacion tinyint NOT NULL,
 	CONSTRAINT pk_c PRIMARY KEY (uNickname, rID),
 	CONSTRAINT fk_cunick FOREIGN KEY (uNickname) references Usuario(uNickname),
 	CONSTRAINT fk_crid FOREIGN KEY (rID) references Receta(rID)
 	
 );
+
+SET FOREIGN_KEY_CHECKS = 1;
--- db/02 Procedures.sql
+++ db/02 Procedures.sql
@@ -1,253 +1,212 @@
-1---Registro usuarios-------------------------------------------------------------------------------
-IF OBJECT_ID('[dbo].[spRegistroUsuario]') IS NULL
-    EXEC('CREATE PROCEDURE [dbo].[spRegistroUsuario] AS;')
-GO
+USE RecetasDivertidasDB;
 
-ALTER PROCEDURE [dbo].[spRegistroUsuario]
+-- Registro de usuarios --
+-- Devuelve un True si el usuario se creó con éxito
+DROP FUNCTION IF EXISTS fnRegistroUsuario;
+DELIMITER //
+CREATE FUNCTION fnRegistroUsuario
 (
-	@uNickname varchar(32),
-	@uPreguntaSeguridad varchar(64),
-	@uRespuestaSeguridad varchar(64),
-	@uNombre varchar,
-	@uApellido varchar,
-	@uContrasenia varchar(50),
-	@uGenero tinyint,
-	@uMail varchar(64)	
+	uNickname varchar(32),
+	uPreguntaSeguridad varchar(64),
+	uRespuestaSeguridad varchar(64),
+	uNombre varchar(50),
+	uApellido varchar(50),
+	uContrasenia varchar(50),
+	uGenero tinyint,
+	uMail varchar(64)	
 )
-AS
+RETURNS boolean
 BEGIN
-	DECLARE @ExisteUsuario int;
-	SET @ExisteUsuario = 
-	(
-		SELECT COUNT(*) FROM Usuario WHERE uNickname = @uNickname
-	);
-	IF @ExisteUsuario = 0
-		BEGIN
-			INSERT INTO Usuario
-			VALUES (@uNickname,@uPreguntaSeguridad,@uRespuestaSeguridad,
-					@uNombre,@uApellido,@uContrasenia,@uGenero,@uMail);
-			SELECT 0 AS 'Result';
-		END
-	ELSE
-		BEGIN
-			SELECT 1 AS 'Result';
-		END
- 
-END
-
-2---Inicio sesion-------------------------------------------------------------------------------
-
-IF OBJECT_ID('[dbo].[spInicioSesion]') IS NULL
-    EXEC('CREATE PROCEDURE [dbo].[spInicioSesion] AS;')
-GO
-
-ALTER PROCEDURE [dbo].[spInicioSesion]
-(	
-	@uNombreUsu varchar(32),
-	@uContra varchar(50)
-	
-)
-AS
-BEGIN
-	DECLARE @BuscarUs int;
-	DECLARE @ContraDB varchar(50);
-	SET @BuscarUs =
+	DECLARE existeUsuario int;
+	SET existeUsuario = 
 		(
-		SELECT COUNT(*) FROM Usuario WHERE uNickname = @uNombreUsu
-		);
-	--Si no encuentra un usuario, devuelve 0--
-	IF @BuscarUs = 0
-		BEGIN
-			SELECT 0 AS 'Result';
-		END
-	--Si encuentra usuario:--
-	ELSE
-		BEGIN
-			SELECT @ContraDB = uContrasenia FROM Usuario WHERE uNickname = @uNombreUsu;
-			--coincide la contrasenia, devuelvue 1--
-			IF @ContraDB = @uContra
-				BEGIN
-					SELECT 1 AS 'Result';
-				END
-			--coincide la contrasenia, devuelve 2--
-			ELSE
-				BEGIN
-					SELECT 2 AS 'Result';
-				END
-		END	
-END
-
-3---Nombres de recetas de un usuario-------------------------------------------------------------------------------
-
-IF OBJECT_ID('[dbo].[spRecetasUsuario]') IS NULL
-    EXEC('CREATE PROCEDURE [dbo].[spRecetasUsuario] AS;')
-GO
-
-ALTER PROCEDURE [dbo].[spRecetasUsuario]
-(
-	@uNickname varchar(32)	
-)
-AS
-BEGIN
-	IF @uNickname IS NULL
-		BEGIN
-			SELECT 0 AS 'Result'	
-		END
-	ELSE
-		BEGIN
-			SELECT rID AS nroReceta, rNombre AS Nombre, rDescripcion AS Descripcion 
-			FROM Receta WHERE uNickname = @uNickname;
-		END
-	
-END
-
-4---Datos de una receta (pasada como parametro por el ID)---------------------------------------------------------------
-
-IF OBJECT_ID('[dbo].[spDatosReceta]') IS NULL
-    EXEC('CREATE PROCEDURE [dbo].[spDatosReceta] AS SELECT -| AS ;')
-GO
-
-ALTER PROCEDURE [dbo].[spDatosReceta]
-(
-	@rID int
-)
-AS
-BEGIN
-	IF @rID IS NULL
-		BEGIN
-			SELECT 0 AS 'Result'	
-		END
-	ELSE
-		BEGIN
-			SELECT 
-				i.iNombre, 
-				ir.cantidad, 
-				ir.unidadCantidad
-			FROM
-				IngredienteReceta ir
-			INNER JOIN
-				Ingrediente i
-			ON i.iID = ir.iID
-			WHERE ir.rID = @rID	
-		END
-END
-
-5----Listar usuarios o datos de un usuario--------------------------------------------
-
-IF OBJECT_ID('[dbo].[spListarUsuarios]') IS NULL
-    EXEC('CREATE PROCEDURE [dbo].[spListarUsuarios] AS ;')
-GO
-ALTER PROCEDURE [dbo].[spDatosUsuario]
-(
-	@uNickname varchar(32) = NULL
-)
-AS
-/*
-Parametro opcional
-Si no se pone:
-	Lista todos los nicknames de los usuarios
-Si se pone:
-	Lista los datos del usuario que tenga el nickanme pasado como parametro
-*/
-BEGIN
-	IF @uNickname IS NULL
-		BEGIN				
-			SELECT
-				uNIckname
-			FROM Usuario
-		END
-	ELSE
-		BEGIN
-			SELECT
-				uNickname,
-				uNombre,
-				uApellido,
-				uGenero,
-				uMail		
-			FROM Usuario
+			SELECT COUNT(*)
+            FROM Usuario
 			WHERE uNickname = @uNickname
-		END
-END
-6---Banear usuario-------------------------------------------------------------------------------
-IF OBJECT_ID('[dbo].[spBaneoUsuario]') IS NULL
-    EXEC('CREATE PROCEDURE [dbo].[spBaneoUsuario] AS ;')
-GO
-ALTER PROCEDURE [dbo].[spBaneoUsuario]
-(
-	@uNickname varchar(32)
-)
-AS
-BEGIN
-	IF @uNickname IS NULL
-		BEGIN
-			SELECT 0 AS 'Result'	
-		END
+		);
+	IF @ExisteUsuario = 0 THEN
+		INSERT INTO Usuario
+		VALUES (uNickname, uPreguntaSeguridad, uRespuestaSeguridad,
+				uNombre, uApellido, uContrasenia, uGenero, uMail);
+		RETURN true;
 	ELSE
-		BEGIN
-			DELETE FROM RecetasDivertidasDB.dbo.Usuario 
-			WHERE uNickname = @uNickname;
-			
-			SELECT 1 AS 'Result'
-		END
-END
-7---Calificar receta-------------------------------------------------------------------------------
-IF OBJECT_ID('[dbo].[spCalificarReceta]') IS NULL
-    EXEC('CREATE PROCEDURE [dbo].[spCalificarReceta] AS ;')
-GO
-ALTER PROCEDURE [dbo].[spCalificarReceta]
-(
-	@rID int,
-	@uNickname varchar(32),
-	@calificacion tinyint
+		RETURN false;
+	END IF;
+ 
+END//
+
+DELIMITER ;
+
+-- Inicio sesion --
+-- Devuelve:
+-- 	0 si el usuario ingresado no existe
+--  1 si el inicio de sesión es exitoso
+--  2 si la contraseña es la incorrecta
+DROP FUNCTION IF EXISTS fnInicioSesion;
+DELIMITER //
+CREATE FUNCTION fnInicioSesion
+(	
+	puNickname varchar(32),
+	puContrasenia varchar(50)
 )
-AS
+RETURNS TINYINT
 BEGIN
-	IF @rID IS NULL OR @uNickname IS NULL OR @calificacion IS NULL
-		BEGIN
-			SELECT 0 AS 'Result'		
-		END
+	DECLARE contraDB varchar(50);
+	DECLARE cantUsuarios int;
+    
+	SELECT COUNT(*)
+    INTO cantUsuarios
+    FROM Usuario
+    WHERE uNickname = puNickname;
+	-- Si no encuentra un usuario, devuelve 0
+	IF cantUsuarios = 0 THEN
+		RETURN 0;
 	ELSE
-		BEGIN
-			INSERT INTO Calificacion
-			VALUES (@uNickname, @rID, @calificacion)
-			SELECT 1 AS 'Result'
-		END
-END
----Insertar categoria-------------------------------------------------------------------------------
-IF OBJECT_ID('[dbo].[spSubirCategoria]') IS NULL
-    EXEC('CREATE PROCEDURE [dbo].[spSubirCategoria] AS ;')
-GO
-ALTER PROCEDURE [dbo].[spSubirCategoria]
+        SELECT uContrasenia
+        INTO contraDB
+        FROM Usuario
+        WHERE uNickname = puNickname
+        LIMIT 1;
+        
+		IF contraDB = puContrasenia THEN
+            RETURN 1;
+		ELSE
+            RETURN 2;
+		END IF;
+	END IF;
+END//
+DELIMITER ;
+
+-- Obtener las id de las recetas de un usuario---
+-- Devuelve -1 si el usuario no existe
+DROP PROCEDURE IF EXISTS spGetRecetasUsuario;
+DELIMITER //
+CREATE PROCEDURE spGetRecetasUsuario
 (
-	@cNombre varchar(42),
-	--0 categoria de ingrediente--
-	--1 categoria de receta--
-	@catRecetaoIngrediente tinyint
+	uNickname varchar(32)
 )
-AS
 BEGIN
-	IF @cNombre IS NULL or @catRecetaoIngrediente IS NULL
-		BEGIN
-			SELECT 0 AS 'Result'
-		END
-	ELSE
-		BEGIN
-			IF @catRecetaoIngrediente = 0
-				BEGIN
-					INSERT INTO CategoriaDeIngrediente (cNombre)
-					VALUES (@cNombre)
-					SELECT 1 AS 'Result'
-				END
-			ELSE IF @catRecetaoIngrediente = 1
-				BEGIN
-					INSERT INTO CategoriaDeReceta (cNombre)
-					VALUES (@cNombre)
-					SELECT 1 AS 'Result'
-				END
-			ELSE
-				BEGIN
-					SELECT 0 AS 'Result'
-				END
-		END
-END
-----------------------------------------------------------------------------------
+	SELECT rID AS nroReceta, rNombre AS Nombre, rDescripcion AS Descripcion 
+	FROM Receta WHERE uNickname = @uNickname;
+END//
+DELIMITER ;
+
+-- Obtener todos los datos de una receta
+-- Devuelve varios resultsets
+-- 1. Datos básicos de la receta
+-- 2. Ingredientes de la receta
+-- 3. Categorías de la receta
+-- 4. Categorías de sus ingredientes
+-- 5. Multimedia
+DROP PROCEDURE IF EXISTS spGetDatosReceta;
+DELIMITER //
+CREATE PROCEDURE spGetDatosReceta
+(
+	prID int
+)
+BEGIN
+	-- Obtener los datos de la receta
+    SELECT *
+    FROM Receta
+    WHERE rID = prID;
+    
+    -- Seleccionar ingredientes
+	SELECT i.iID, i.iNombre
+	FROM
+		Ingrediente i,
+		IngredienteReceta ir,
+		Receta r
+	WHERE
+		r.rID = prID AND
+		r.rID = ir.rID AND
+		i.iID = ir.iID;	
+	
+    -- Seleccionar categorías de la receta
+    SELECT c.cID, c.cNombre
+    FROM
+		CategoriaDeReceta c,
+        RelCatReceta cr,
+        Receta r
+	WHERE
+		c.cID = cr.cID AND
+        r.rID = cr.rID AND
+        r.rID = prID;
+	
+    -- Seleccionar categorías los ingredientes
+	SELECT DISTINCT
+		c.cID,
+        c.cNombre
+    FROM
+		CategoriaDeIngrediente c,
+        RelCatIngrediente cr,
+        Ingrediente i,
+        IngredienteReceta ir
+	WHERE
+		i.iID = cr.iID AND
+        c.cID = cr.cID AND
+        i.iID = ir.iID AND
+        r.rID = ir.rID AND
+        ir.rID = prID;
+	
+    -- Seleccionar multimedia
+    SELECT mID, link
+	FROM Multimedia
+    WHERE rID = prID;
+END//
+DELIMITER ;
+
+-- Listar usuarios o datos de un usuario --
+DROP PROCEDURE IF EXISTS spDatosUsuario;
+DELIMITER //
+CREATE PROCEDURE spDatosUsuario
+(
+	puNickname varchar(32)
+)
+BEGIN
+	SELECT
+		uNickname,
+		uNombre,
+		uApellido,
+		uGenero,
+		uMail,
+		uHabilitado
+	FROM Usuario
+	WHERE uNickname = puNickname;
+END//
+DELIMITER ;
+
+-- Banear usuario --
+DROP PROCEDURE IF EXISTS spBaneoUsuario;
+DELIMITER //
+CREATE PROCEDURE spBaneoUsuario
+(
+	puNickname varchar(32)
+)
+BEGIN
+	UPDATE Usuario
+    SET uHabilitado = false
+    WHERE uNickname = puNichname;
+END//
+DELIMITER ;
+
+-- Calificar receta --
+DROP PROCEDURE IF EXISTS spCalificarReceta;
+DELIMITER //
+CREATE PROCEDURE spCalificarReceta
+(
+	prID int,
+	puNickname varchar(32),
+	pcalificacion tinyint
+)
+BEGIN
+	-- Se borra la calificación anterior (si es que la tiene)
+    -- por si el usuario quiso cambiarla
+	DELETE FROM Calificacion
+    WHERE
+		rID = prID AND
+        uNickname = puNickname;
+	
+    INSERT INTO Calificacion
+    VALUES (puNickname, prID, pcalificacion);
+END//
+DELIMITER ;
