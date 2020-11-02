c921c02a7c375380a3a7eab2ecc8af5f6b370a3b
Caruffa <camilolaruffa@gmail.com>
2020-11-02 14:23:44 -0300

    Subir recetas se ve más fachero

--- cliente/src/main/java/io/github/recetasDivertidas/pkgSubir/SubirLayout.java
+++ cliente/src/main/java/io/github/recetasDivertidas/pkgSubir/SubirLayout.java
@@ -6,6 +6,7 @@ import io.github.recetasDivertidas.pkgRecetasDivertidas.CategoriaReceta;
 import io.github.recetasDivertidas.pkgRecetasDivertidas.Ingrediente;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
@@ -13,53 +14,67 @@ import org.controlsfx.control.CheckComboBox;
 
 import java.io.IOException;
 
-public class SubirLayout extends GridPane {
+public class SubirLayout extends BorderPane {
 
     public SubirLayout() throws IOException {
-        this.setPadding(new Insets(10, 10, 10, 10));
-        this.setVgap(8);
-        this.setHgap(10);
-        this.setGridLinesVisible(false);
-
-
         Label lblTitulo = new Label("Conoces una receta y quieres añadirla? Subela aqui!");
         lblTitulo.setFont(new Font("Arial",16));
-        this.add(lblTitulo, 0, 0,1,1);
+
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(getGridpane());
+
+        this.setTop(lblTitulo);
+        this.setCenter(scrollPane);
+    }
+
+    private GridPane getGridpane() throws IOException {
+        GridPane gridPane = new GridPane();
+
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setVgap(8);
+        gridPane.setHgap(10);
+        gridPane.setGridLinesVisible(false);
+
+
+
 
         Label lblCategoria = new Label("Que tipo de comida es?");
         lblCategoria.setFont(new Font("Arial",16));
-        this.add(lblCategoria, 1, 1,1,1);
+        gridPane.add(lblCategoria, 1, 1,1,1);
 
         CheckComboBox<CategoriaReceta> cmbCategoriaReceta = new CheckComboBox<>();
         if (Conexion.isSvResponse()) {
             cmbCategoriaReceta.getItems().addAll(SubirFuncionalidad.getCategoriasReceta());
         }
-        this.add(cmbCategoriaReceta,2,1,1,1);
+        gridPane.add(cmbCategoriaReceta,2,1,1,1);
 
         TextField tbTitulo = new TextField();
         tbTitulo.setPrefSize(200, 10);
         tbTitulo.setPromptText("Titulo de la receta");
-        this.add(tbTitulo, 0, 1,1,1);
+        gridPane.add(tbTitulo, 0, 1,1,1);
 
         TextArea tbDescripcion = new TextArea();
         tbDescripcion.setPrefSize(200,50);
         tbDescripcion.setPromptText("Descripcion de la receta");
-        this.add(tbDescripcion,0,2,3,1);
+        gridPane.add(tbDescripcion,0,2,3,1);
 
         TextArea tbInstrucciones = new TextArea();
         tbInstrucciones.setPrefSize(800,400);
         tbInstrucciones.setPromptText("Instrucciones de la receta");
-        this.add(tbInstrucciones,0,3,3,1);
+        gridPane.add(tbInstrucciones,0,3,3,1);
 
         //Tengo que meter esto en un ScrollPane y añadir un boton de "+" y otro de "-" para añadir y sacar ingredientes
         HBox hboxIng = getHbox();
-        this.add(hboxIng,0,4,1,1);
+        gridPane.add(hboxIng,0,4,1,1);
 
 
         Button btnSubirReceta = new Button("Subir Receta");
         btnSubirReceta.setOnAction(e -> SubirFuncionalidad.subirReceta());
 
-        this.add(btnSubirReceta,0,5,1,1);
+        gridPane.add(btnSubirReceta,0,5,1,1);
+
+
+        return gridPane;
     }
 
     private HBox getHbox() throws IOException {
