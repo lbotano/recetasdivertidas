33e2c950f2376d3732966601b39c79187cb61313
Caruffa <camilolaruffa@gmail.com>
2020-11-17 16:34:31 -0300

    Arreglado Issue

--- cliente/src/main/java/io/github/recetasDivertidas/pkgBusquedas/BusquedaIngredientes.java
+++ cliente/src/main/java/io/github/recetasDivertidas/pkgBusquedas/BusquedaIngredientes.java
@@ -1,8 +1,14 @@
 package io.github.recetasDivertidas.pkgBusquedas;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import org.controlsfx.control.CheckComboBox;
 
 public class BusquedaIngredientes {
+    @FXML Button btnBuscar;
+    @FXML CheckComboBox chkcmbIngredientes;
+
     public void nextPag(ActionEvent actionEvent) {
     }
 
