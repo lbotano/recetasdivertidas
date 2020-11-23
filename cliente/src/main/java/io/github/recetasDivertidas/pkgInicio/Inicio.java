1ae9c153b4bee17f2642567464654876cb65ed48
Caruffa <camilolaruffa@gmail.com>
2020-11-23 16:44:14 -0300

    Implementados nextPag y prevPag en top recetas

--- cliente/src/main/java/io/github/recetasDivertidas/pkgInicio/Inicio.java
+++ cliente/src/main/java/io/github/recetasDivertidas/pkgInicio/Inicio.java
@@ -3,27 +3,20 @@ package io.github.recetasDivertidas.pkgInicio;
 import io.github.recetasDivertidas.pkgAplicacion.Alerta;
 import io.github.recetasDivertidas.pkgComponentes.ResultadoBusqueda;
 import io.github.recetasDivertidas.pkgRecetasDivertidas.Receta;
-import io.github.recetasDivertidas.pkgRecetasDivertidas.RecetasDivertidas;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import org.controlsfx.glyphfont.FontAwesome;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class Inicio{
     @FXML Button btnNextPag;
     @FXML Button btnPrevPag;
     @FXML VBox vboxResultados;
-    private int current_pag = 0;
+    private int paginaActual = 0;
 
     ArrayList<Receta> topRecetas;
 
@@ -42,7 +35,7 @@ public class Inicio{
         vboxResultados.getChildren().clear();
 
         try {
-            topRecetas = Receta.getTopRecetas(current_pag);
+            topRecetas = Receta.getTopRecetas(paginaActual);
 
             for (Receta r : topRecetas) {
                 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/componentes/resultado_busqueda.fxml"));
@@ -60,14 +53,18 @@ public class Inicio{
         }
     }
 
-    @FXML
-    private void nextPag() {
-        // TODO: Implementar
+    public void nextPag() {
+        if(vboxResultados.getChildren().size() == 10){
+            paginaActual++;
+            actualizarRecetas();
+        }
     }
 
-    @FXML
-    private void prevPag() {
-        // TODO: Implementar
+    public void prevPag() {
+        if (paginaActual > 0) {
+            paginaActual--;
+            actualizarRecetas();
+        }
     }
 
 }
