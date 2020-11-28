a2485194180a9da1329624902458e430efa70c89
Caruffa <camilolaruffa@gmail.com>
2020-11-28 14:31:45 -0300

    Bien puestos los poblar tablas

---
+++ db/3.1 Poblar tablas categoria receta.sql
@@ -0,0 +1,19 @@
+-- Añadir categorías de recetas
+CALL spAgregarCategoriaReceta("Almuerzo"); -- 1
+CALL spAgregarCategoriaReceta("Postre"); -- 2
+CALL spAgregarCategoriaReceta("Cena"); -- 3
+CALL spAgregarCategoriaReceta("Desayuno"); -- 4
+CALL spAgregarCategoriaReceta("Merienda"); -- 5
+CALL spAgregarCategoriaReceta("Colación"); -- 6
+CALL spAgregarCategoriaReceta("Panes o masas"); -- 7
+CALL spAgregarCategoriaReceta("Snack"); -- 8
+CALL spAgregarCategoriaReceta("Bebida"); -- 9
+CALL spAgregarCategoriaReceta("Sopa o caldo"); -- 10
+CALL spAgregarCategoriaReceta("Aperitivo"); -- 11
+CALL spAgregarCategoriaReceta("Plato principal"); -- 12
+CALL spAgregarCategoriaReceta("Ensalada"); -- 13
+CALL spAgregarCategoriaReceta("Guarnición"); -- 14
+CALL spAgregarCategoriaReceta("Salsa"); -- 15
+CALL spAgregarCategoriaReceta("Al horno"); -- 16
+CALL spAgregarCategoriaReceta("A parrilla"); -- 17
+CALL spAgregarCategoriaReceta("Pasta"); -- 18
---
+++ db/3.2 Poblar tablas categoria ingredientes.sql
@@ -0,0 +1,19 @@
+-- Añadir categorías de ingredientes
+CALL spAgregarCategoriaIngrediente("Vegano"); -- 1
+CALL spAgregarCategoriaIngrediente("Celíaco"); -- 2
+CALL spAgregarCategoríaIngrediente("Vegetariano"); -- 3
+CALL spAgregarCategoríaIngrediente("Con azucar"); -- 4
+CALL spAgregarCategoríaIngrediente("Carne vacuna"); -- 5
+CALL spAgregarCategoríaIngrediente("Carne porcina"); -- 6
+CALL spAgregarCategoríaIngrediente("Carne ovina"); -- 7
+CALL spAgregarCategoríaIngrediente("Carne de pollo"); -- 8
+CALL spAgregarCategoríaIngrediente("Pescado"); -- 9
+CALL spAgregarCategoríaIngrediente("Derivado lacteo"); -- 10
+CALL spAgregarCategoríaIngrediente("Legumbre"); -- 11
+CALL spAgregarCategoríaIngrediente("Verdura"); -- 12
+CALL spAgregarCategoríaIngrediente("Fruta"); -- 13
+CALL spAgregarCategoríaIngrediente("Fruto seco"); -- 14
+CALL spAgregarCategoríaIngrediente("Cereal o derivado"); -- 15
+CALL spAgregarCategoríaIngrediente("Grasa"); -- 16
+CALL spAgregarCategoríaIngrediente("Hierba aromática"); -- 17
+CALL spAgregarCategoríaIngrediente("Especia"); -- 18
