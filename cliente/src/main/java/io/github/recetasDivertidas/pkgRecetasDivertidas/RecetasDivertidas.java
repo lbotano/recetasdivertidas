94b018551cb62c368055d2d27b3e0748239136e1
Caruffa <camilolaruffa@gmail.com>
2020-11-29 14:56:47 -0300

    Termine los packetes

--- cliente/src/main/java/io/github/recetasDivertidas/pkgRecetasDivertidas/RecetasDivertidas.java
+++ cliente/src/main/java/io/github/recetasDivertidas/pkgRecetasDivertidas/RecetasDivertidas.java
@@ -1,25 +1,12 @@
 package io.github.recetasDivertidas.pkgRecetasDivertidas;
-import io.github.recetasDivertidas.pkgAplicacion.Alerta;
-import io.github.recetasDivertidas.pkgAplicacion.Aplicacion;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.util.Hashtable;
 
 public class RecetasDivertidas {
     public static boolean logueadoComoAdmin;
